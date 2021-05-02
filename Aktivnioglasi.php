1458fe445118a8452275f1cc731b43860b310297
Jon Rojnik <jon.rojnik@scv.si>
2021-05-02 19:38:31 +0200

    izpis zasedenih casov za oglase

--- Aktivnioglasi.php
+++ Aktivnioglasi.php
@@ -20,7 +20,7 @@ $oglasiarray = baza::zasedenioglasi();
     foreach ($oglasiarray as $oglas) {
 
         $id = baza::idOglasa($oglas->model,$oglas->znamka,$oglas->letnik,$oglas->cena,$oglas->imeuporabnika,$oglas->priimek,$oglas->kraj,$oglas->pot);
-        echo $id;
+        $casiarray = baza::zasedenicasi($id);
         echo "<tr>";
         echo "<td>".$oglas->znamka."</td>";
         echo "<td>".$oglas->model."</td>";
@@ -29,9 +29,16 @@ $oglasiarray = baza::zasedenioglasi();
         echo "<td>".$oglas->cena."</td>";
         echo "<td>".$oglas->kraj."</td>";
         echo "<td>".$oglas->pot."</td>";
+        echo "<td>";
+        foreach($casiarray as $cas)
+        {
+            $casova = explode("|",$cas);
+            $odcas = explode("+",$casova[0]);
+            $docas = explode("+",$casova[1]);
+            echo "Od: ".$odcas[0]." Do: ".$docas[0]."<br>";
+        }
+        echo "</td>";
         echo "</tr>";
-
-
     }
 
     ?>
--- baza.php
+++ baza.php
@@ -28,7 +28,7 @@ avtomobili a ON a.id_modela = m.id_m INNER JOIN oglasi o ON o.id_avtomobila = a.
     static function zasedenioglasi()
     {
         $zasedeno = array();
-        $result = pg_query(self::con(),"SELECT m.ime_m,z.ime_z,a.letnik,o.cena_ura,u.ime_u,u.priimek_u,k.ime_k,a.pot_slike FROM znamke z INNER JOIN modeli m ON m.id_znamke = z.id_z INNER JOIN
+        $result = pg_query(self::con(),"SELECT DISTINCT m.ime_m,z.ime_z,a.letnik,o.cena_ura,u.ime_u,u.priimek_u,k.ime_k,a.pot_slike FROM znamke z INNER JOIN modeli m ON m.id_znamke = z.id_z INNER JOIN
 avtomobili a ON a.id_modela = m.id_m INNER JOIN oglasi o ON o.id_avtomobila = a.id_a INNER JOIN uporabniki u ON u.id_u = o.id_uporabnika INNER JOIN kraji k ON k.id_k = o.id_kraja INNER JOIN
 zaseden_cas zc ON o.id_o = zc.id_oglasa WHERE kon_datum > NOW();");
         $x = 0;
@@ -46,7 +46,7 @@ zaseden_cas zc ON o.id_o = zc.id_oglasa WHERE kon_datum > NOW();");
     static function idOglasa($imem,$imez,$letnik,$cena,$imeu,$priimek,$kraj,$pot)
     {
 
-        $result = pg_query(self::con(),"SELECT o.id_o FROM znamke z INNER JOIN modeli m ON m.id_znamke = z.id_z INNER JOIN
+        $result = pg_query(self::con(),"SELECT  o.id_o FROM znamke z INNER JOIN modeli m ON m.id_znamke = z.id_z INNER JOIN
 avtomobili a ON a.id_modela = m.id_m INNER JOIN oglasi o ON o.id_avtomobila = a.id_a INNER JOIN uporabniki u ON u.id_u = o.id_uporabnika INNER JOIN kraji k ON k.id_k = o.id_kraja
 WHERE z.ime_z = '$imez' AND m.ime_m = '$imem' AND a.letnik = $letnik AND u.ime_u = '$imeu' AND u.priimek_u = '$priimek'
     AND o.cena_ura = $cena AND k.ime_k = '$kraj' AND a.pot_slike = '$pot';");
@@ -58,14 +58,22 @@ WHERE z.ime_z = '$imez' AND m.ime_m = '$imem' AND a.letnik = $letnik AND u.ime_u
         return $idar;
     }
 
-    /*
+
     //zasedeni casi za posamezni oglas
-    static function zasedenicasi()
+    static function zasedenicasi($idog)
     {
         $casi = array();
-        $result = pg_query(self::con(),"")
+        $result = pg_query(self::con(),"SELECT zac_datum,kon_datum FROM zaseden_cas WHERE id_oglasa = $idog AND kon_datum > NOW();");
+        $x = 0;
+        while($row = pg_fetch_row($result))
+        {
+            $casi[$x] = $row[0]."|".$row[1];
+            $x++;
+        }
+        pg_close();
+        return $casi;
     }
-    */
+
 
 
 
