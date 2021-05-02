7ca9e268bbb62359c56fa4cb6e2d5f6b64e7530d
Jon Rojnik <jon.rojnik@scv.si>
2021-05-02 16:08:17 +0200

    povezava z bazo

--- baza.php
+++ baza.php
@@ -7,6 +7,7 @@ Class baza
         return pg_connect("host=ec2-52-19-170-215.eu-west-1.compute.amazonaws.com dbname=dal6j2j2bi233e user=ooenbkgvbwqtco password=b26c71535815417b340efb10b2d2a4914b6bdb03a2dc611b9381e76b794bf457");
     }
 
+    //izpis oglasov
     static function selectoglasi()
     {
         $oglasi = array();
