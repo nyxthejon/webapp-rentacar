a7268d01c31a1927f104e901a290423654c6f122
Jon Rojnik <jon.rojnik@scv.si>
2021-05-03 16:29:32 +0200

    aws downloads bucket images

--- aws.php
+++ aws.php
@@ -17,17 +17,33 @@ $manager = new \Aws\S3\Transfer($client, $source, $dest);
 $manager->transfer();
 
 */
-$s3 = S3Client::factory(array(
-    'key' => 'AKIAZE5W2FP77YGXVEWB',
-    'secret' => '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',
+$s3Client = new S3Client([
     'region' => 'eu-central-1',
     'version' => 'latest',
-    'profile' => 'default'
+    'credentials' => [
+    'key' => 'AKIAZE5W2FP77YGXVEWB',
+        'secret' => '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'
+        ],
 
-));
+]);
+
+$buckets = $s3Client->listBuckets();
+foreach ($buckets['Buckets'] as $bucket) {
+}
+
+// Where the files will be sourced from
+$source = 's3://'.$bucket['Name'];
+
+// Where the files will be transferred to
+$dest = 'C:\xampp\htdocs\webapp\img';
+
+$manager = new \Aws\S3\Transfer($s3Client, $source, $dest);
+$manager->transfer();
+/*
 
 $objects = $s3 -> getIterator('ListObjects',array(
-    'Bucket' => $config['s3']
+    'Bucket' => $bucket['Name'];
 ));
 var_dump($objects);
 
+*/
